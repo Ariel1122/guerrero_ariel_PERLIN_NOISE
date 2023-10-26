ef37c6af5581602f1a92f78ef3f03c752d0dc0d0
Ariel Guerrero <ariel.guerrero@ucuenca.edu.ec>
2023-10-26 06:58:47 -0500

    agregado el map que estaba pendiente dentro del noise

--- sketch.js
+++ sketch.js
@@ -9,15 +9,15 @@ function setup() {
 function draw() {
   background(0, 5); //para "desvanecer" el fondo con una baja opacidad, lo que crea un efecto de trazo persistente
 
-  let x = width * noise(t);
-  let y = height * noise(t + 5); //modificando la entrada que se utiliza para calcular el valor del ruido de Perlin en un momento dado. La función
+  let x = map(noise(t), 1, 0, 0, width);
+  let y = map(noise(t + 5), 1, 0, 0, height); //modificando la entrada que se utiliza para calcular el valor del ruido de Perlin en un momento dado. La función
 
-  let tmn = 100 * noise(t); //La función noise(¿) genera un valor de ruido Perlin en función de la variable xoff, el varlor del perlin se trnaforma gracias la multiplicacion en 100.
-  let tmnI = 50 * noise(t + 10); // Tamaño de la pelota interior,  la variable t se incrementa en cada fotograma con la línea, A medida que t aumenta, la entrada en la función noise() cambia. Como resultado, el valor de ruido generado también cambia, creando un efecto de animación en constante cambio.
+  let tmn = map(noise(t), 1, 0, 0, 100); //La función noise(¿) genera un valor de ruido Perlin en función de la variable xoff, el varlor del perlin se trnaforma gracias la multiplicacion en 100.
+  let tmnI = map(noise(t + 10), 1, 0, 0, 50); // Tamaño de la pelota interior,  la variable t se incrementa en cada fotograma con la línea, A medida que t aumenta, la entrada en la función noise() cambia. Como resultado, el valor de ruido generado también cambia, creando un efecto de animación en constante cambio.
 
-  let r = 255 * noise(t + 10); //modifica la entrada de la función, lo que a su vez afecta la suavidad y la variación del ruido de Perlin. Esto es lo que permite que los elementos (en este caso, las elipses) se muevan y cambien de color de manera aparentemente aleatoria
-  let g = 255 * noise(t + 15);
-  let b = 255 * noise(t + 20);
+  let r = map(noise(t + 10), 1, 0, 0, 255); //modifica la entrada de la función, lo que a su vez afecta la suavidad y la variación del ruido de Perlin. Esto es lo que permite que los elementos (en este caso, las elipses) se muevan y cambien de color de manera aparentemente aleatoria
+  let g = map(noise(t + 15), 1, 0, 0, 255);
+  let b = map(noise(t + 20), 1, 0, 0, 255);
 
   push(); // Guardar la configuración de transformación actual
   translate(x, y); // Mover el origen de coordenadas al centro de la figura
